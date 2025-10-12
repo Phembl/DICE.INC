173446169549023a76b2d6741dfc753db8f89fd1
Phembl <max@selvi.xyz>
2025-10-12 11:43:12 +0200

    Changed Positions of Lab & Import
    Added Menu Buttons

---
+++ Assets/_DICE INC/Code/Buttons/Button_Menu_Message.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Button_Menu_Message : Button
+{
+    void Start()
+    {
+        isActive = true;
+    }
+
+    protected override void ButtonAction()
+    {
+        // Settings -> Message
+    }
+}
